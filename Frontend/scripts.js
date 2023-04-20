349a7c6a42748d31c3d5e37cfa6a2696d9c2d1dc
Sushil <70800396+Sushilkoirala@users.noreply.github.com>
2023-04-20 12:26:24 +0200

    Update scripts.js

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -1,45 +1,101 @@
-// Get references to the HTML elements
+const inputImage = document.getElementById('input-image');
 const imagePreview = document.getElementById('image-preview');
-const petNameOutput = document.getElementById('pet-name');
-const latencyOutput = document.getElementById('latency');
+const canvas = document.getElementById('canvas');
+const category = document.getElementById('category');
+const subcategory = document.getElementById('subcategory');
+const confidence = document.getElementById('confidence');
+const location = document.getElementById('location');
+const objectCount = document.getElementById('object-count');
 const clearButton = document.getElementById('clear-button');
-const submitButton = document.getElementById('submit-button');
+const detectButton = document.getElementById('detect-button');
+const progressBar = document.getElementById('progress-bar');
 
-// Set up a function to capture the image from the camera and display it on the page
-function captureImage() {
-    // TODO: Add code here to capture the image from the Raspberry Pi camera and set the 'src' attribute of the 'imagePreview' element to the captured image
+let model;
+let predictions = [];
+
+// Load the TensorFlow.js model
+async function loadModel() {
+    model = await tf.loadGraphModel('model/model.json');
 }
 
-// Add an event listener to the 'clear' button to clear the output fields
-clearButton.addEventListener('click', () => {
-    petNameOutput.textContent = '';
-    latencyOutput.textContent = '';
-});
+// Clear the canvas and output container
+function clearOutput() {
+    predictions = [];
+    category.innerText = '';
+    subcategory.innerText = '';
+    confidence.innerText = '';
+    location.innerText = '';
+    objectCount.innerText = '';
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+}
 
-// Add an event listener to the 'submit' button to send the image to the Django view for image recognition
-submitButton.addEventListener('click', () => {
-    // Get the image data from the 'imagePreview' element
-    const imageData = imagePreview.getAttribute('src');
+// Draw the bounding box around the detected object
+function drawBoundingBox(prediction) {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.lineWidth = '2';
+    ctx.strokeStyle = 'red';
+    ctx.rect(prediction.bbox[0], prediction.bbox[1], prediction.bbox[2], prediction.bbox[3]);
+    ctx.stroke();
+}
 
-    // Create a new FormData object to send the image data to the server
-    const formData = new FormData();
-    formData.append('image', imageData);
+// Update the output container with the details of the detected object(s)
+function updateOutput() {
+    if (predictions.length > 0) {
+        // Display the details of the first detected object
+        category.innerText = predictions[0].class;
+        subcategory.innerText = predictions[0].subclass;
+        confidence.innerText = `${(predictions[0].score * 100).toFixed(2)}%`;
+        location.innerText = `[${predictions[0].bbox[0]}, ${predictions[0].bbox[1]}, ${predictions[0].bbox[2]}, ${predictions[0].bbox[3]}]`;
+        objectCount.innerText = predictions.length.toString();
+        // Draw the bounding box around the first detected object
+        drawBoundingBox(predictions[0]);
+    } else {
+        // Clear the output container if no objects were detected
+        clearOutput();
+    }
+}
 
-    // Send a POST request to the server with the image data
-    fetch('/recognize-image', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Update the output fields with the pet name and latency
-        petNameOutput.textContent = data.petName;
-        latencyOutput.textContent = `Latency: ${data.latency}ms`;
-    })
-    .catch(error => {
-        console.error(error);
+// Detect objects in the input image
+async function detectObjects() {
+    // Clear the previous output
+    clearOutput();
+
+    // Get the input image data
+    const input = tf.browser.fromPixels(imagePreview);
+    const imageHeight = input.shape[0];
+    const imageWidth = input.shape[1];
+
+    // Preprocess the image
+    const preprocessedInput = tf.image.resizeBilinear(input, [300, 300]).div(255.0).expandDims();
+    
+    // Run the model on the preprocessed image
+    const predictionsTensor = await model.predict(preprocessedInput).array();
+    predictions = tf.util.squeeze(predictionsTensor, 0);
+
+    // Filter out low-confidence detections
+    const threshold = 0.5;
+    predictions = predictions.filter(prediction => prediction.score > threshold);
+
+    // Convert the bounding box coordinates from normalized values to pixel values
+    predictions.forEach(prediction => {
+        prediction.bbox[0] *= imageWidth;
+        prediction.bbox[1] *= imageHeight;
+        prediction.bbox[2] *= imageWidth;
+        prediction.bbox[3] *= imageHeight;
     });
+
+    // Update the output container
+    updateOutput();
+}
+
+// Load the model on page load
+window.addEventListener('load', () => {
+    loadModel();
 });
 
-// Call the 'captureImage' function when the page loads
-captureImage();
+// Handle input image change
+inputImage.addEventListener('change', () => {
+    const file = inputImage.files[0];
+    if (file) {
+        // Display the selected
