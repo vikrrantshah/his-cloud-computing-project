6f0486b699325e7c88eb5a3a005c6a1e9d4440a5
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2023-05-17 01:06:27 +0200

    data read & write ready

--- pet-detection/pages/api/index.ts
+++ pet-detection/pages/api/index.ts
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const prisma = new PrismaClient();
   if (req.method === "POST") {
-    return res.status(200).json({ name: "John Doe" });
+    const data = req.body;
+    const captureData = await prisma.captureData.create({ data });
+    return res.status(200).json({ data: captureData });
   }
   if (req.method === "GET") {
-    return res.status(200).json({ name: "John Doe" });
+    const data = await prisma.captureData.findMany();
+    return res.status(200).json(data);
   }
   return res.status(405).json({ error: "Method not allowed" });
 }
