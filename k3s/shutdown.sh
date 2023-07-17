4115e5077f4606a20ec3d5d093063ec9954d0f30
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2023-07-17 14:10:56 +0200

    update

--- k3s/shutdown.sh
+++ k3s/shutdown.sh
@@ -1,5 +1,5 @@
 sudo kubectl delete deployment pet-detection-web \
 && sudo kubectl delete svc pet-detection-service \
-&& sudo kubectl delete deployment minio \
-&& sudo kubectl delete pvc minio-volume \
-&& sudo kubectl delete svc minio-service
+&& sudo kubectl delete deployment storage-deployment \
+&& sudo kubectl delete pvc storage-volume \
+&& sudo kubectl delete svc storage-service
